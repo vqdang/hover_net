accf52343d50b319270a640bf5c31b98a61471ce
Simon Graham <simon_graham@hotmail.co.uk>
2019-09-27 13:45:32 +0100

    update line 64

--- src/extract_patches.py
+++ src/extract_patches.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
             # merge classes for CoNSeP (in paper we only utilise 3 nuclei classes and background)
             # If own dataset is used, then the below may need to be modified
             ann_type[(ann_type == 3) | (ann_type == 4)] = 3
-            ann_type[(ann_type == 5) | (ann_type == 6)] = 4
+            ann_type[(ann_type == 5) | (ann_type == 6) | (ann_type == 7)] = 4
             
             ann = np.dstack([ann_inst, ann_type])
             ann = ann.astype('int32')
