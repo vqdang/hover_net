37fde06542b3dc8f5204450b763e59a97906e83e
vqdang <dangvuquoc1993@gmail.com>
2019-10-29 03:20:18 +0900

    static ordering

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -51,8 +51,9 @@ def get_files(data_dir_list, data_ext):
 
     data_files = []
     for sub_dir in data_dir_list:
-        files = glob.glob(sub_dir + '/*'+ data_ext)
-        data_files.extend(files)
+        files_list = glob.glob(sub_dir + '/*'+ data_ext)
+        files_list.sort() # ensure same order
+        data_files.extend(files_list)
 
     return data_files
 
