2eb967864bf88d5eb3fddc87468d041e9f3855bf
vqdang <dangvuquoc1993@gmail.com>
2020-06-19 15:04:27 +0000

    migrate

--- dataloader/train_loader.py
+++ dataloader/train_loader.py
@@ -9,14 +9,17 @@ import numpy as np
 import scipy.io as sio
 import torch.utils.data
 
+import imgaug as ia
 from imgaug import augmenters as iaa
-import albumentations as A
 
-from misc.utils import cropping_center, center_pad_to_shape
-from .augs import gen_instance_hv_map
+from misc.utils import center_pad_to_shape, cropping_center
 
+from .augs import (add_to_brightness, add_to_contrast, add_to_hue,
+                   add_to_saturation, gaussian_blur, gen_instance_hv_map,
+                   median_blur)
 
-class TrainSerialLoader(torch.utils.data.Dataset):
+####
+class FileLoader(torch.utils.data.Dataset):
     """
     Data Loader. Loads images from a file list and 
     performs augmentation with the albumentation library.
@@ -29,54 +32,70 @@ class TrainSerialLoader(torch.utils.data.Dataset):
         mask_shape: shape of the output [h,w] - defined in config.py
         mode: 'train' or 'valid'
     """
-    def __init__(self, file_list, input_shape=None, mask_shape=None, mode='train'):
+    def __init__(self, file_list, input_shape=None, mask_shape=None, 
+                mode='train', setup_augmentor=True):
         assert input_shape is not None and mask_shape is not None
+        self.mode = mode
         self.mask_shape = mask_shape
         self.input_shape = input_shape
-        self.augment_img_mask, self.augment_img = self.__augmentation__(mode)
         self.info_list = file_list
+        self.id = 0
+        if setup_augmentor:
+            self.setup_augmentor(0)
+        return
+
+    def setup_augmentor(self, worker_id):
+        seed = np.random.randint(0, 2**32) + worker_id
+        self.augmentor = self.__get_augmentation(self.mode, seed)
+        self.shape_augs = iaa.Sequential(self.augmentor[0]) 
+        self.input_augs = iaa.Sequential(self.augmentor[1]) 
+        self.id = self.id + worker_id
+        return
 
     def __len__(self):
         return len(self.info_list)
 
     def __getitem__(self, idx):
+        # print(idx)
         path = self.info_list[idx]
         data = np.load(path)
 
         # split stacked channel into image and label
-        img = (data[..., :3]).astype('uint8')  # RGB images
-        ann = data[..., 3:]  # instance ID map and type map
+        img = (data[...,:3]).astype('uint8') # RGB images
+        ann = data[...,3:] # instance ID map and type map
 
-        # perform augmentation
-        aug_img_mask = self.augment_img_mask(image=img, mask=ann)
-        img = aug_img_mask['image']
-        ann = aug_img_mask['mask']
+        if self.shape_augs is not None:
+            shape_augs = self.shape_augs.to_deterministic()
+            img = shape_augs.augment_image(img)
+            ann = shape_augs.augment_image(ann)
 
-        aug_img = self.augment_img(image=img)
-        img = aug_img['image']
-
-        feed_dict = {'img': img}
-        # * Specific on-the-fly processing for annotation label
-        if ann.shape[-1] == 2:  # nuclei segmentation + classification
+        if self.input_augs is not None:
+            input_augs = self.input_augs.to_deterministic()
+            img = input_augs.augment_image(img)
+        
+        feed_dict = {'img' : img}
+        # * Specific on the flight processing for annotation label
+        if False and ann.shape[-1] == 2: # Nuclei Segmentation + Type Classification
             inst_map, type_map = np.dsplit(ann, -1)
             np_map = np.array(inst_map > 0, dtype='uint8')
             hv_map = gen_instance_hv_map(inst_map, self.mask_shape)
-            # binarise instance map, ordering of operaton matters !
+            # binarize instance map, ordering of operaton matters !
             np_map = cropping_center(np_map, self.mask_shape)
             hv_map = cropping_center(hv_map, self.mask_shape)
             tp_map = cropping_center(type_map, self.mask_shape)
-            feed_dict['np_map'] = np_map[..., None]  # HWC
+            feed_dict['np_map'] = np_map[...,None] # HWC
             feed_dict['hv_map'] = hv_map
-            feed_dict['tp_map'] = tp_map[..., None]  # HWC
-        else:  # nuclei segmentation only
-            inst_map = ann[..., 0]  # HW1 -> HW
+            feed_dict['tp_map'] = tp_map[...,None] # HWC
+        else: # Nuclei Segmentation only
+            inst_map = ann[...,0] # HW1 -> HW
             np_map = np.array(inst_map > 0, dtype='uint8')
             hv_map = gen_instance_hv_map(inst_map, self.mask_shape)
             np_map = cropping_center(np_map, self.mask_shape)
             hv_map = cropping_center(hv_map, self.mask_shape)
-            feed_dict['np_map'] = np_map[..., None]  # HWC
+            feed_dict['np_map'] = np_map[...,None] # HWC
             feed_dict['hv_map'] = hv_map
 
+        # print(img.shape, ann.shape)
         return feed_dict
 
     @staticmethod
@@ -112,51 +131,63 @@ class TrainSerialLoader(torch.utils.data.Dataset):
             displayed_sample = prep_sample(sample_data)
             plt.subplot(batch_size, 1, sample_idx+1)
             plt.imshow(displayed_sample)
-        plt.show()
+        # plt.show()
+        plt.savefig('dump.png', dpi=600)
         return
 
-    def __augmentation__(self, mode):
-        """
-        Augmentation pipeline. For more information on how this can be modified, refer to:
-        https://albumentations.readthedocs.io/en/latest/api/augmentations.html#module-albumentations.augmentations.transforms
-        """
+    def __get_augmentation(self, mode, rng):
         if mode == 'train':
-            augment_img_mask = A.Compose([
-                A.ShiftScaleRotate(shift_limit=5, scale_limit=0.2,
-                                   rotate_limit=179, interpolation=cv2.INTER_NEAREST),
-                A.VerticalFlip(p=.5),
-                A.HorizontalFlip(p=.5),
-                A.ElasticTransform(alpha_affine=0, alpha=35,
-                                   sigma=5, interpolation=cv2.INTER_NEAREST),
-                A.CenterCrop(self.input_shape[0], self.input_shape[1])
-            ])
-
-            augment_img = A.Compose([
-                # different ordering will give different results (not commutative)
-                A.OneOf([
-                    A.Compose([
-                        A.HueSaturationValue(hue_shift_limit=25,
-                                             sat_shift_limit=20, val_shift_limit=0, p=1),
-                        A.RandomBrightnessContrast(
-                            brightness_limit=0.2, contrast_limit=0.2, p=1)
-                    ]),
-                    A.Compose([
-                        A.RandomBrightnessContrast(
-                            brightness_limit=0.2, contrast_limit=0.2, p=1),
-                        A.HueSaturationValue(hue_shift_limit=25,
-                                             sat_shift_limit=20, val_shift_limit=0, p=1)
-                    ])
+            shape_augs = [
+                # * order = ``0`` -> ``cv2.INTER_NEAREST``
+                # * order = ``1`` -> ``cv2.INTER_LINEAR``
+                # * order = ``2`` -> ``cv2.INTER_CUBIC``
+                # * order = ``3`` -> ``cv2.INTER_CUBIC``
+                # * order = ``4`` -> ``cv2.INTER_CUBIC``
+                iaa.Affine(
+                    # scale images to 80-120% of their size, individually per axis
+                    scale={"x": (0.8, 1.2), 
+                           "y": (0.8, 1.2)}, 
+                    # translate by -A to +A percent (per axis)
+                    translate_percent={"x": (-0.01, 0.01), 
+                                       "y": (-0.01, 0.01)}, 
+                    shear=(-5, 5), # shear by -5 to +5 degrees
+                    rotate=(-179, 179), # rotate by -179 to +179 degrees
+                    order=0,    # use nearest neighbour
+                    backend='cv2', # opencv for fast processing
+                    seed=rng
+                ),
+                # set position to 'center' for center crop
+                # else 'uniform' for random crop
+                iaa.CropToFixedSize(self.input_shape[0], 
+                                    self.input_shape[1],
+                                    position='center'
+                ),
+                iaa.Fliplr(0.5, seed=rng),
+                iaa.Flipud(0.5, seed=rng),
+            ]
+        
+            input_augs = [
+                iaa.OneOf([
+                    iaa.Lambda(seed=rng, func_images= lambda *args : gaussian_blur(*args, max_ksize=3)),
+                    iaa.Lambda(seed=rng, func_images= lambda *args : median_blur(*args, max_ksize=3)),
+                    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
                 ]),
-                A.OneOf([A.MedianBlur(blur_limit=3),
-                         A.GaussianBlur(blur_limit=3),
-                         A.GaussNoise(var_limit=0.05*255)
-                         ])
-            ])
+                iaa.Sequential([
+                    iaa.Lambda(seed=rng, func_images= lambda *args : add_to_hue(*args, range=(-8, 8))),
+                    iaa.Lambda(seed=rng, func_images= lambda *args : add_to_saturation(*args, range=(-0.2, 0.2))),
+                    iaa.Lambda(seed=rng, func_images= lambda *args : add_to_brightness(*args, range=(-26, 26))),
+                    iaa.Lambda(seed=rng, func_images= lambda *args : add_to_contrast(*args, range=(0.75, 1.25))),
+                ], random_order=True),
+            ]   
+        elif mode == 'valid':
+            shape_augs = [
+                # set position to 'center' for center crop
+                # else 'uniform' for random crop
+                iaa.CropToFixedSize(self.input_shape[0], 
+                                    self.input_shape[1],
+                                    position='center')
+            ]
+            input_augs = [
+            ]
 
-        else:
-            augment_img_mask = A.Compose([
-                A.CenterCrop(self.input_shape[0], self.input_shape[1])
-            ])
-            augment_img = A.NoOp()
-
-        return augment_img_mask, augment_img
+        return shape_augs, input_augs
