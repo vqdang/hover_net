a172b5260814e38d5039475b7d0a373921ee9dfe
vqdang <dangvuquoc1993@gmail.com>
2020-12-18 11:51:13 +0000

    UPD: update pretrained path

--- models/hovernet/opt.py
+++ models/hovernet/opt.py
@@ -52,7 +52,7 @@ def get_config(nr_type, mode):
                         },
                         # path to load, -1 to auto load checkpoint from previous phase,
                         # None to start from scratch
-                        "pretrained": "../pretrained/ImageNet-ResNet50-Preact-Pytorch.npz",
+                        "pretrained": "../pretrained/ImageNet-ResNet50-Preact_pytorch.tar",
                         # 'pretrained': None,
                     },
                 },
