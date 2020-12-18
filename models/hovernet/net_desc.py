91b6416c112b232d9f8d2c2c77c995b217a1e4bd
vqdang <dangvuquoc1993@gmail.com>
2020-12-18 11:50:56 +0000

    UPD: sync naming and refactor

--- models/hovernet/net_desc.py
+++ models/hovernet/net_desc.py
@@ -1,312 +1,35 @@
-import numpy as np
 import math
+from collections import OrderedDict
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from collections import OrderedDict
-
+from .net_utils import (DenseBlock, Net, ResidualBlock, TFSamepaddingLayer,
+                        UpSample2x)
 from .utils import crop_op, crop_to_shape
-
-
-####
-class Net(nn.Module):
-    """ A base class provides a common weight initialisation scheme."""
-
-    def weights_init(self):
-        for m in self.modules():
-            classname = m.__class__.__name__
-
-            # ! Fixed the type checking
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-
-            if "norm" in classname.lower():
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-            if "linear" in classname.lower():
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        return x
-
-
-####
-class TFSamepaddingLayer(Net):
-    """To align with tf `same` padding. 
-    
-    Putting this before any conv layer that need padding
-    Assuming kernel has Height == Width for simplicity.
-
-    """
-
-    def __init__(self, ksize, stride):
-        super().__init__()
-        self.ksize = ksize
-        self.stride = stride
-
-    def forward(self, x):
-        if x.shape[2] % self.stride == 0:
-            pad = max(self.ksize - self.stride, 0)
-        else:
-            pad = max(self.ksize - (x.shape[2] % self.stride), 0)
-
-        if pad % 2 == 0:
-            pad_val = pad // 2
-            padding = (pad_val, pad_val, pad_val, pad_val)
-        else:
-            pad_val_start = pad // 2
-            pad_val_end = pad - pad_val_start
-            padding = (pad_val_start, pad_val_end, pad_val_start, pad_val_end)
-        x = F.pad(x, padding, "constant", 0)
-        return x
-
-
-####
-class DenseBlock(Net):
-    """Dense Block as defined in:
-
-    Huang, Gao, Zhuang Liu, Laurens Van Der Maaten, and Kilian Q. Weinberger. 
-    "Densely connected convolutional networks." In Proceedings of the IEEE conference 
-    on computer vision and pattern recognition, pp. 4700-4708. 2017.
-
-    Only performs `valid` convolution.
-
-    """
-
-    def __init__(self, in_ch, unit_ksize, unit_ch, unit_count, split=1):
-        super().__init__()
-        assert len(unit_ksize) == len(unit_ch), "Unbalance Unit Info"
-
-        self.nr_unit = unit_count
-        self.in_ch = in_ch
-        self.unit_ch = unit_ch
-
-        # ! For inference only so init values for batchnorm may not match tensorflow
-        unit_in_ch = in_ch
-        self.units = nn.ModuleList()
-        for idx in range(unit_count):
-            self.units.append(
-                nn.Sequential(
-                    OrderedDict(
-                        [
-                            ("preact_bn", nn.BatchNorm2d(unit_in_ch, eps=1e-5)),
-                            ("preact_relu", nn.ReLU(inplace=True)),
-                            (
-                                "conv1",
-                                nn.Conv2d(
-                                    unit_in_ch,
-                                    unit_ch[0],
-                                    unit_ksize[0],
-                                    stride=1,
-                                    padding=0,
-                                    bias=False,
-                                ),
-                            ),
-                            ("conv1_bn", nn.BatchNorm2d(unit_ch[0], eps=1e-5)),
-                            ("conv1_relu", nn.ReLU(inplace=True)),
-                            (
-                                "conv2",
-                                nn.Conv2d(
-                                    unit_ch[0],
-                                    unit_ch[1],
-                                    unit_ksize[1],
-                                    groups=split,
-                                    stride=1,
-                                    padding=0,
-                                    bias=False,
-                                ),
-                            ),
-                        ]
-                    )
-                )
-            )
-            unit_in_ch += unit_ch[1]
-
-        self.blk_bna = nn.Sequential(
-            OrderedDict(
-                [
-                    ("bn", nn.BatchNorm2d(unit_in_ch, eps=1e-5)),
-                    ("relu", nn.ReLU(inplace=True)),
-                ]
-            )
-        )
-
-    def forward(self, prev_feat):
-        for idx in range(self.nr_unit):
-            new_feat = self.units[idx](prev_feat)
-            prev_feat = crop_to_shape(prev_feat, new_feat)
-            prev_feat = torch.cat([prev_feat, new_feat], dim=1)
-        prev_feat = self.blk_bna(prev_feat)
-
-        return prev_feat
-
-
-####
-class ResidualBlock(Net):
-    """Residual block as defined in:
-
-    He, Kaiming, Xiangyu Zhang, Shaoqing Ren, and Jian Sun. "Deep residual learning 
-    for image recognition." In Proceedings of the IEEE conference on computer vision 
-    and pattern recognition, pp. 770-778. 2016.
-
-    """
-
-    def __init__(self, in_ch, unit_ksize, unit_ch, unit_count, stride=1):
-        super().__init__()
-        assert len(unit_ksize) == len(unit_ch), "Unbalance Unit Info"
-
-        self.nr_unit = unit_count
-        self.in_ch = in_ch
-        self.unit_ch = unit_ch
-
-        # ! For inference only so init values for batchnorm may not match tensorflow
-        unit_in_ch = in_ch
-        self.units = nn.ModuleList()
-        for idx in range(unit_count):
-            unit_layer = [
-                ("preact_bn", nn.BatchNorm2d(unit_in_ch, eps=1e-5)),
-                ("preact_relu", nn.ReLU(inplace=True)),
-                (
-                    "conv1",
-                    nn.Conv2d(
-                        unit_in_ch,
-                        unit_ch[0],
-                        unit_ksize[0],
-                        stride=1,
-                        padding=0,
-                        bias=False,
-                    ),
-                ),
-                ("conv1_bn", nn.BatchNorm2d(unit_ch[0], eps=1e-5)),
-                ("conv1_relu", nn.ReLU(inplace=True)),
-                (
-                    "conv2_pad",
-                    TFSamepaddingLayer(
-                        ksize=unit_ksize[1], stride=stride if idx == 0 else 1
-                    ),
-                ),
-                (
-                    "conv2",
-                    nn.Conv2d(
-                        unit_ch[0],
-                        unit_ch[1],
-                        unit_ksize[1],
-                        stride=stride if idx == 0 else 1,
-                        padding=0,
-                        bias=False,
-                    ),
-                ),
-                ("conv2_bn", nn.BatchNorm2d(unit_ch[1], eps=1e-5)),
-                ("conv2_relu", nn.ReLU(inplace=True)),
-                (
-                    "conv3",
-                    nn.Conv2d(
-                        unit_ch[1],
-                        unit_ch[2],
-                        unit_ksize[2],
-                        stride=1,
-                        padding=0,
-                        bias=False,
-                    ),
-                ),
-            ]
-            # * has bna to conclude each previous block so
-            # * must not put preact for the first unit of this block
-            unit_layer = unit_layer if idx != 0 else unit_layer[2:]
-            self.units.append(nn.Sequential(OrderedDict(unit_layer)))
-            unit_in_ch = unit_ch[-1]
-
-        if in_ch != unit_ch[-1] or stride != 1:
-            self.shortcut = nn.Conv2d(in_ch, unit_ch[-1], 1, stride=stride, bias=False)
-        else:
-            self.shortcut = None
-
-        self.blk_bna = nn.Sequential(
-            OrderedDict(
-                [
-                    ("bn", nn.BatchNorm2d(unit_in_ch, eps=1e-5)),
-                    ("relu", nn.ReLU(inplace=True)),
-                ]
-            )
-        )
-
-    def out_ch(self):
-        return self.unit_ch[-1]
-
-    def forward(self, prev_feat, freeze=False):
-        if self.shortcut is None:
-            shortcut = prev_feat
-        else:
-            shortcut = self.shortcut(prev_feat)
-
-        for idx in range(0, len(self.units)):
-            new_feat = prev_feat
-            if self.training:
-                with torch.set_grad_enabled(not freeze):
-                    new_feat = self.units[idx](new_feat)
-            else:
-                new_feat = self.units[idx](new_feat)
-            prev_feat = new_feat + shortcut
-            shortcut = prev_feat
-        feat = self.blk_bna(prev_feat)
-        return feat
-
-
-####
-class UpSample2x(nn.Module):
-    """Upsample input by a factor of 2.
-    
-    Assume input is of NCHW, port FixedUnpooling from TensorPack.
-    """
-
-    def __init__(self):
-        super().__init__()
-        # correct way to create constant within module
-        self.register_buffer(
-            "unpool_mat", torch.from_numpy(np.ones((2, 2), dtype="float32"))
-        )
-        self.unpool_mat.unsqueeze(0)
-
-    def forward(self, x):
-        input_shape = list(x.shape)
-        # unsqueeze is expand_dims equivalent
-        # permute is transpose equivalent
-        # view is reshape equivalent
-        x = x.unsqueeze(-1)  # bchwx1
-        mat = self.unpool_mat.unsqueeze(0)  # 1xshxsw
-        ret = torch.tensordot(x, mat, dims=1)  # bxcxhxwxshxsw
-        ret = ret.permute(0, 1, 2, 4, 3, 5)
-        ret = ret.reshape((-1, input_shape[1], input_shape[2] * 2, input_shape[3] * 2))
-        return ret
-
 
 ####
 class HoVerNet(Net):
     """Initialise HoVer-Net."""
 
-    def __init__(self, input_ch=3, nr_types=None, freeze=False):
+    def __init__(self, input_ch=3, nr_types=None, freeze=False, mode='original'):
         super().__init__()
+        self.mode = mode
         self.freeze = freeze
         self.nr_types = nr_types
         self.output_ch = 3 if nr_types is None else 4
 
-        self.conv0 = nn.Sequential(
-            OrderedDict(
-                [
-                    (
-                        "conv",
-                        nn.Conv2d(input_ch, 64, 7, stride=1, padding=0, bias=False),
-                    ),
-                    ("bn", nn.BatchNorm2d(64, eps=1e-5)),
-                    ("relu", nn.ReLU(inplace=True)),
-                ]
-            )
-        )
+        module_list = [
+            ("/", nn.Conv2d(input_ch, 64, 7, stride=1, padding=0, bias=False)),
+            ("bn", nn.BatchNorm2d(64, eps=1e-5)),
+            ("relu", nn.ReLU(inplace=True)),
+        ]
+        if mode == 'fast': # prepend the padding for `fast` mode
+            module_list = [("pad", TFSamepaddingLayer(ksize=7, stride=1))] + module_list
 
+        self.conv0 = nn.Sequential(OrderedDict(module_list))
         self.d0 = ResidualBlock(64, [1, 3, 1], [64, 64, 256], 3, stride=1)
         self.d1 = ResidualBlock(256, [1, 3, 1], [128, 128, 512], 4, stride=2)
         self.d2 = ResidualBlock(512, [1, 3, 1], [256, 256, 1024], 6, stride=2)
@@ -314,73 +37,46 @@ class HoVerNet(Net):
 
         self.conv_bot = nn.Conv2d(2048, 1024, 1, stride=1, padding=0, bias=False)
 
-        def create_decoder_branch(out_ch=2):
-            u3 = nn.Sequential(
-                OrderedDict(
-                    [
-                        (
-                            "conva",
-                            nn.Conv2d(1024, 256, 5, stride=1, padding=0, bias=False),
-                        ),
-                        ("dense", DenseBlock(256, [1, 5], [128, 32], 8, split=4)),
-                        (
-                            "convf",
-                            nn.Conv2d(512, 512, 1, stride=1, padding=0, bias=False),
-                        ),
-                    ]
-                )
-            )
-            u2 = nn.Sequential(
-                OrderedDict(
-                    [
-                        (
-                            "conva",
-                            nn.Conv2d(512, 128, 5, stride=1, padding=0, bias=False),
-                        ),
-                        ("dense", DenseBlock(128, [1, 5], [128, 32], 4, split=4)),
-                        (
-                            "convf",
-                            nn.Conv2d(256, 256, 1, stride=1, padding=0, bias=False),
-                        ),
-                    ]
-                )
-            )
-            u1 = nn.Sequential(
-                OrderedDict(
-                    [
-                        ("conva_pad", TFSamepaddingLayer(ksize=5, stride=1)),
-                        (
-                            "conva",
-                            nn.Conv2d(256, 64, 5, stride=1, padding=0, bias=False),
-                        ),
-                    ]
-                )
-            )
+        def create_decoder_branch(out_ch=2, ksize=5):
+            module_list = [ 
+                ("conva", nn.Conv2d(1024, 256, ksize, stride=1, padding=0, bias=False)),
+                ("dense", DenseBlock(256, [1, ksize], [128, 32], 8, split=4)),
+                ("convf", nn.Conv2d(512, 512, 1, stride=1, padding=0, bias=False),),
+            ]
+            u3 = nn.Sequential(OrderedDict(module_list))
 
-            u0 = nn.Sequential(
-                OrderedDict(
-                    [
-                        ("bn", nn.BatchNorm2d(64, eps=1e-5)),
-                        ("relu", nn.ReLU(inplace=True)),
-                        (
-                            "conv",
-                            nn.Conv2d(64, out_ch, 1, stride=1, padding=0, bias=True),
-                        ),
-                    ]
-                )
-            )
+            module_list = [ 
+                ("conva", nn.Conv2d(512, 128, ksize, stride=1, padding=0, bias=False)),
+                ("dense", DenseBlock(128, [1, ksize], [128, 32], 4, split=4)),
+                ("convf", nn.Conv2d(256, 256, 1, stride=1, padding=0, bias=False),),
+            ]
+            u2 = nn.Sequential(OrderedDict(module_list))
+
+            module_list = [ 
+                ("conva/pad", TFSamepaddingLayer(ksize=ksize, stride=1)),
+                ("conva", nn.Conv2d(256, 64, ksize, stride=1, padding=0, bias=False),),
+            ]
+            u1 = nn.Sequential(OrderedDict(module_list))
+
+            module_list = [ 
+                ("bn", nn.BatchNorm2d(64, eps=1e-5)),
+                ("relu", nn.ReLU(inplace=True)),
+                ("conv", nn.Conv2d(64, out_ch, 1, stride=1, padding=0, bias=True),),
+            ]
+            u0 = nn.Sequential(OrderedDict(module_list))
 
             decoder = nn.Sequential(
                 OrderedDict([("u3", u3), ("u2", u2), ("u1", u1), ("u0", u0),])
             )
             return decoder
 
+        ksize = 5 if mode == 'original' else 3
         if nr_types is None:
             self.decoder = nn.ModuleDict(
                 OrderedDict(
                     [
-                        ("np", create_decoder_branch(out_ch=2)),
-                        ("hv", create_decoder_branch(out_ch=2)),
+                        ("np", create_decoder_branch(ksize=ksize,out_ch=2)),
+                        ("hv", create_decoder_branch(ksize=ksize,out_ch=2)),
                     ]
                 )
             )
@@ -388,9 +84,9 @@ class HoVerNet(Net):
             self.decoder = nn.ModuleDict(
                 OrderedDict(
                     [
-                        ("tp", create_decoder_branch(out_ch=nr_types)),
-                        ("np", create_decoder_branch(out_ch=2)),
-                        ("hv", create_decoder_branch(out_ch=2)),
+                        ("tp", create_decoder_branch(ksize=ksize, out_ch=nr_types)),
+                        ("np", create_decoder_branch(ksize=ksize, out_ch=2)),
+                        ("hv", create_decoder_branch(ksize=ksize, out_ch=2)),
                     ]
                 )
             )
@@ -422,8 +118,12 @@ class HoVerNet(Net):
             d = [d0, d1, d2, d3]
 
         # TODO: switch to `crop_to_shape` ?
-        d[0] = crop_op(d[0], [184, 184])
-        d[1] = crop_op(d[1], [72, 72])
+        if self.mode == 'original':
+            d[0] = crop_op(d[0], [184, 184])
+            d[1] = crop_op(d[1], [72, 72])
+        else:
+            d[0] = crop_op(d[0], [92, 92])
+            d[1] = crop_op(d[1], [36, 36])
 
         out_dict = OrderedDict()
         for branch_name, branch_desc in self.decoder.items():
@@ -444,11 +144,7 @@ class HoVerNet(Net):
 
 ####
 def create_model(mode=None, **kwargs):
-    if mode == "original":
-        return HoVerNet(**kwargs)
-    elif mode == "fast":
-        from .net_desc_fast import HoVerNet as HoVerNetFast
-        return HoVerNetFast(**kwargs)
-    else:
+    if mode not in ['original', 'fast']:
         assert "Unknown Model Mode %s" % mode
+    return HoVerNet(mode=mode, **kwargs)
 
