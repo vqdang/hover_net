4978aa5e578c2e32982a3d54197270360630dc4a
vqdang <dangvuquoc1993@gmail.com>
2021-01-10 13:41:50 +0000

    FIX: remove debug code

--- run_infer.py
+++ run_infer.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
             'save_mask'      : sub_args['save_mask'],
         })
     # ***
-    exit()
+    
     if sub_cmd == 'tile':
         from infer.tile import InferManager
         infer = InferManager(**method_args)
